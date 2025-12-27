ea83aab7c850c11e039cfe9239acfe42c4894304
jcisek <cisekjoseph@gmail.com>
2025-12-27 13:26:41 -0700

    0.0.1.2
    Working it out

--- Build/Downloads.loader.js
+++ Build/Downloads.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 67644772,
+    wasmFileSize: 67645652,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
